042adcd4f35820a998b55191dbae4785f3bbeec8
Makoto Yoshida <milmed1013@gmail.com>
2021-12-09 13:27:11 +0000

    add order repository

---
+++ laravel/packages/infrastructures/persistance/orders/OrderRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Packages\Infrastructures\Persistance\Orders;
+
+use App\Models\OrderModel;
+use Illuminate\Support\LazyCollection;
+use Packages\Domains\Orders\Order;
+
+class OrderRepository
+{
+    public function find(int $orderId): Order
+    {
+        return OrderModel::find($orderId)->toEntity();
+    }
+
+    /**
+     * @return LazyCollection<Order>
+     */
+    public function fetchAll(): LazyCollection
+    {
+        return OrderModel::cursor()
+            ->map(fn (OrderModel $m) => $m->toEntity())
+        ;
+    }
+
+    /**
+     * @return int オートインクリメント採番されたID
+     */
+    public function insert(Order $order): int
+    {
+        return $order->toModel()->create()->id;
+    }
+
+    /**
+     * @return boolean 更新成功:true / 失敗:false
+     */
+    public function update(Order $order): bool
+    {
+        return $order->toModel()->save();
+    }
+}
